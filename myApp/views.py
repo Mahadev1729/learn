aa2c53aaedd635b8875d913fa1789881d9bd2803
Mahadev1729 <devcampus17@gmail.com>
2025-07-29 15:45:49 +0530

    feat: add Feature model and update index view to display features

--- myApp/views.py
+++ myApp/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 # Create your views here.
 def index(request):
-    context={
-        'name':'Mahadev',
-        'age':23,
-        'nationality':'India'
-    }
-    return render(request,'index.html',context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is very quick'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Secure'
+    feature2.details = 'We keep your data safe and private'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Reliable'
+    feature3.details = 'Our service is always available and trustworthy'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'User-Friendly'
+    feature4.details = 'Our interface is simple and easy to use'
+    
+    features=[feature1,feature2,feature3,feature4]
+    return render(request,'index.html',{'features':features})
 
 def counter(request):
     text=request.POST['text']
