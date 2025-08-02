ea9d68b70d71e6bc37194f2097ec8be2dd830a7c
Mahadev1729 <devcampus17@gmail.com>
2025-08-02 16:46:45 +0530

    registration

--- myApp/views.py
+++ myApp/views.py
@@ -1,33 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Feature
-# Create your views here.
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+
 def index(request):
-    # feature1 = Feature()
-    # feature1.id = 0
-    # feature1.name = 'Fast'
-    # feature1.is_true=True
-    # feature1.details = 'Our service is very quick'
-
-    # feature2 = Feature()
-    # feature2.id = 1
-    # feature2.name = 'Secure'
-    # feature2.is_true=True
-    # feature2.details = 'We keep your data safe and private'
-
-    # feature3 = Feature()
-    # feature3.id = 2
-    # feature3.name = 'Reliable'
-    # feature3.is_true=False
-    # feature3.details = 'Our service is always available and trustworthy'
-
-    # feature4 = Feature()
-    # feature4.id = 3
-    # feature4.name = 'User-Friendly'
-    # feature4.is_true=True
-    # feature4.details = 'Our interface is simple and easy to use'
-    
-    # features=[feature1,feature2,feature3,feature4]
     features=Feature.objects.all()
     return render(request,'index.html',{'features':features})
 
@@ -35,3 +13,24 @@ def counter(request):
     text=request.POST['text']
     amout_of_words=len(text.split())
     return render(request,'counter.html',{'amount':amout_of_words})
+
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+        
+        
+        if password==password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username already used')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                return redirect('login')
+    return render(request,'register.html')
